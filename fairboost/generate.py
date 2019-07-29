61cb299b0726faf1e7adc774d83ad6d3c41a4201
mzgubic <miha.zgubic@gmail.com>
2019-07-29 16:24:05 +0200

    add sklearn bdt plus toy example

---
+++ fairboost/generate.py
@@ -0,0 +1,74 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def generate_toys(n_samples, z=None, train=True):
+    """
+    Args:
+        n_samples (int): number of examples to generate
+        z (None or float): generate events at a particular value of z, by default depends on the label
+        
+    Returns a tuple (X, Y, Z) of arrays. In each array, a row is an instance.
+    """
+    
+    sigma = 1.0
+
+    # labels Y: first half are zeros, second half are ones
+    Y = np.zeros(n_samples)
+    Y[n_samples//2:] = 1
+
+    # protected parameter Z
+    # in the training set (z=None) z depends on the label
+    # (i.e. mass distribution depends on signal/background)
+    if z == None:
+        Z0 = np.zeros(n_samples//2)
+        Z1 = np.random.normal(0, 1, size=n_samples//2)
+    # or generate at specific value of Z (for evaluation purposes)
+    else:
+        Z0 = z * np.ones(n_samples//2)
+        Z1 = z * np.ones(n_samples//2)
+    Z = np.concatenate([Z0, Z1])
+
+    # feature X depend on the value of Z (i.e. kinematics depends on the mass)
+    X0 = np.random.multivariate_normal([0, 0], [[sigma, -.5*sigma], [-.5*sigma, sigma]], size=n_samples//2)
+    X1 = np.random.multivariate_normal([1, 1], 0.5*np.eye(2), size=n_samples//2)
+    X1[:,1] += Z1
+    X = np.concatenate([X0, X1])
+
+    return X, Y, Z
+
+def show_variates(ax, generate):
+    """
+    Plots the random variates.
+    
+    Args:
+        ax (matplotlib axis): Axis on which to plot the variates.
+        generate (function): function to generate the variates.
+    """
+    
+    # generate
+    n_samples = 5000
+    X0, Y0, Z0 = generate(n_samples, z=0)
+    Y0 = Y0.ravel()
+    Z0 = Z0.ravel()
+    X1, Y1, Z1 = generate(n_samples, z=1)
+    Y1 = Y1.ravel()
+    Z1 = Z1.ravel()
+    X_1, Y_1, Z_1 = generate(n_samples, z=-1)
+    Y_1 = Y_1.ravel()
+    Z_1 = Z_1.ravel()
+    
+    # plot
+    ax.scatter(X0[Y0==0,0], X0[Y0==0,1], marker='o', color='k', alpha=1.0, label='Y=0')
+    ax.scatter(X_1[Y_1==1,0], X_1[Y_1==1,1], marker='x', c='tomato', alpha=0.3, label='Y=1, Z=-1')
+    ax.scatter(X0[Y0==1,0], X0[Y0==1,1], marker='x', c='red', alpha=0.3, label='Y=1, Z=0')
+    ax.scatter(X1[Y1==1,0], X1[Y1==1,1], marker='x', c='darkred', alpha=0.3, label='Y=1, Z=1')
+    
+    
+    # cosmetics
+    ax.set_ylim(-1, 3)
+    ax.set_xlim(-1, 2)
+    ax.set_xlabel('x1')
+    ax.set_ylabel('x2')
+    leg = ax.legend(loc='best')
+    ax.set_title('Variates')
+
---
+++ fairboost/plot.py
@@ -0,0 +1,90 @@
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve
+
+def show_roc_curves(ax, clf, generate):
+
+    # generate test data
+    n_samples = 10000
+    X0, Y0, Z0 = generate(n_samples, z=0)
+    X1, Y1, Z1 = generate(n_samples, z=1)
+    X_1, Y_1, Z_1 = generate(n_samples, z=-1)
+
+    # compute the roc curves
+    Y0_pred = clf.predict_proba(X0)[:,1]
+    fpr0, tpr0, _ = roc_curve(Y0, Y0_pred)
+
+    Y1_pred = clf.predict_proba(X1)[:,1]
+    fpr1, tpr1, _ = roc_curve(Y1, Y1_pred)
+
+    Y_1_pred = clf.predict_proba(X_1)[:,1]
+    fpr_1, tpr_1, _ = roc_curve(Y_1, Y_1_pred)
+
+    # draw the roc curves
+    ax.plot(fpr_1, tpr_1, c='tomato', label='Z=-1')
+    ax.plot(fpr0, tpr0, c='red', label='Z=0')
+    ax.plot(fpr1, tpr1, c='darkred', label='Z=1')
+
+    # cosmetics
+    ax.legend(loc='best')
+    ax.set_xlabel('False positive rate')
+    ax.set_ylabel('True positive rate')
+    ax.set_title('ROC curve')
+
+def show_decision_boundary(ax, clf, generate):
+
+    # generate test data
+    n_samples = 100000
+    X, Y, Z = generate(n_samples)
+
+    # predict
+    preds = clf.predict_proba(X)[:,1]
+
+    # plot
+    dec = ax.tricontourf(X[:,0], X[:,1], preds.ravel(), 20, extend='both')
+    plt.colorbar(dec, ax=ax)
+
+    # cosmetics
+    ax.set_ylim(-1, 3)
+    ax.set_xlim(-1, 2)
+    ax.set_title('Decision boundary')
+    ax.set_xlabel('x1')
+    ax.set_ylabel('x2')
+
+
+def show_clf_output(ax, clf, generate):
+
+    # generate test data
+    n_samples = 10000
+    X0, Y0, Z0 = generate(n_samples, z=0)
+    X1, Y1, Z1 = generate(n_samples, z=1)
+    X_1, Y_1, Z_1 = generate(n_samples, z=-1)
+
+    # compute the predictions
+    Y0_pred = clf.predict_proba(X0)[:,1]
+    Y1_pred = clf.predict_proba(X1)[:,1]
+    Y_1_pred = clf.predict_proba(X_1)[:,1]
+
+    # show them
+    bins = 30
+    ax.hist(Y1_pred, bins=bins, density=True, color='darkred', histtype='step', label='Z=1')
+    ax.hist(Y0_pred, bins=bins, density=True, color='red', histtype='step', label='Z=0')
+    ax.hist(Y_1_pred, bins=bins, density=True, color='tomato', histtype='step', label='Z=-1')
+
+    # cosmetics
+    ax.legend(loc='best')
+    ax.set_title('Classifier output')
+    ax.set_xlabel('Classifier output f(X|Z=z)')
+    
+def show_clf(clf, generate):
+    
+    # create the figure
+    fig, ax  = plt.subplots(1, 3, figsize=(15, 5))
+
+    # roc ruve
+    show_roc_curves(ax[0], clf, generate)
+    
+    # decision boundary
+    show_decision_boundary(ax[1], clf, generate)
+    
+    # classifier output
+    show_clf_output(ax[2], clf, generate)
